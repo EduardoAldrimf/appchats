6eafa575dab874dc522c69290f9d18b10b89554e
EduardoAldrimf <152108577+EduardoAldrimf@users.noreply.github.com>
2024-07-01 01:28:46 -0300

    Update dashboard_app.rb
    Fix (tirai dashboard fixed).

--- app/models/dashboard_app.rb
+++ app/models/dashboard_app.rb
@@ -2,14 +2,13 @@
 #
 # Table name: dashboard_apps
 #
-#  id               :bigint           not null, primary key
-#  content          :jsonb
-#  title            :string           not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  account_id       :bigint           not null
-#  user_id          :bigint
-#  dashboard_fixed  :boolean          default(FALSE)  # Atualização para incluir o novo atributo
+#  id         :bigint           not null, primary key
+#  content    :jsonb
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  account_id :bigint           not null
+#  user_id    :bigint
 #
 # Indexes
 #
